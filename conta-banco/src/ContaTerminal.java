19344ecc984fcb07334bd648ee1703baf1551ca1
Porto888 <guilhermepstk@hotmail.com>
2024-05-30 20:29:29 -0300

    Entrega da primeira etapa da atividade

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,36 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner Scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Olá,Bem vindo, ao XPTO Bank");
+
+
+        System.out.println("Por favor, digite o seu nome");
+        String nome = Scanner.next();
+
+//        System.out.println("Agora digite o seu sobrenome");
+//        String sobrenome = Scanner.next();
+
+        System.out.println("Por favor, digite o número da sua agencia");
+        String agencia = Scanner.next();
+
+        System.out.println("Digite por favor o número da sua conta");
+        int conta = Scanner.nextInt();
+
+        double saldo1 = 1000.00;
+
+
+         System.out.println("saldo");
+          double saldo = Scanner.nextDouble();
+
+        // Output input by user
+        System.out.println("Olá, " + nome + " obrigado por retornar ao XPTO Bank, " + "sua agência é: " + agencia +
+                " a conta é, " + conta + " e o saldo disponível é de: R$" + saldo);
+
+
     }
 }
+
